e6f76312c74bef4b565398e21280fa01a25ba7a0
FrontTestSite <testsitewinc@gmail.com>
2020-10-22 13:54:55 +0300

    init: task resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = String(n).split('');
+    let arr = [];
+
+    for(let i = str.length - 1; i >= 0; i-- ) {
+        if(str[i] !== '-') {
+            arr.push(str[i])
+        }
+    }
+
+    return  parseFloat( arr.join(''));
+};
